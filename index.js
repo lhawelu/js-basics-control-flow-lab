9f83a185e99261390a4791fce7255bc5a9ad63a1
Larry Hawelu <lhawelu@gmail.com>
2021-02-22 12:40:43 -0800

    commit tree 19757022712efc4c9b9dfe486fb500bb25533f13

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return "This one is on me!"
+  } else if (ride <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
+};
+
+function ternaryCheckCity(city){
+  const isNyc = "Ok, sounds good."
+  const notNYC = "No go."
+  if (city === "NYC") {
+    return isNyc
+  } else {
+    return notNYC
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  const generousResponse = "Thank you so much."
+  const notGenerousResponse = "Thank you."
+  switch (tip) {
+    case "generous":
+      return generousResponse;
+    case "not as generous":
+      return notGenerousResponse;
+    default:
+      return "Bye."
+  }
 }
